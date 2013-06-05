50089dd3f5c4da02e69de40d767ad5ced93ced79
Taichi Kiwaki <taichi@vindaloo.local>
2013-06-05 21:35:49 +0900

    2nd 1st rev

--- 2nd/test.py
+++ 2nd/test.py
@@ -9,24 +9,27 @@ N = 50;
 x = 2*np.pi*rand(N);
 y = np.sin(x) + 0.3*randn(N);
 #print(np.sin(np.arange(1,11)))
-print(y)
+
 
 tics = 2*np.pi*np.arange(0,1,0.02);
-
-fig1 = plt.figure()
-lw = 5 #line width
-plt.xlim(0,2*np.pi)
-plt.ylim(-1.5,1.5)
-plt.plot(x,y, 'ro', ms=10)
-plt.plot(tics,np.sin(tics), linewidth=lw)
-
 
 phi = np.transpose(np.mat([x, np.power(x, 2), np.power(x, 3), np.power(x, 4), np.power(x, 5)]));
 phid = LA.inv(np.transpose(phi)*phi)*np.transpose(phi);
 wml = phid*np.transpose(np.mat(y));
 
 xx = np.array(np.transpose(wml)*np.mat([tics, np.power(tics, 2), np.power(tics, 3), np.power(tics, 4), np.power(tics, 5)]))
-print(xx[0])
-plt.plot(tics,xx[0], linewidth=lw);
 
+#draw results
+fig1 = plt.figure()
+lw = 5 #line width
+plt.xlim(0,2*np.pi)
+plt.ylim(-1.5,1.5)
+plt.plot(x,y, 'ro', ms=10)
+plt.plot(tics,np.sin(tics), linewidth=lw)
+plt.plot(tics,xx[0], linewidth=lw);
 plt.show()
+#print(y)
+#print(xx[0])
+
+
+
