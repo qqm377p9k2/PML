75c9041905f11998f28d51837e83ef6a8c94e6b3
Taichi Kiwaki <taichi@vindaloo.local>
2013-07-04 01:36:34 +0900

    lasso 2

--- 2nd/lasso.py
+++ 2nd/lasso.py
@@ -18,14 +18,19 @@ y = np.mat(np.sin(x) + 0.3*randn(N));
 
 tics = 2*np.pi*np.arange(0,1,0.02);
 
-training_steps = 10000;
-wml = 0.0001*np.mat(randn(M,1));
+training_steps = 500000;
+wml = np.mat(randn(M,1));
 
 phi = np.mat(map(mkBasis(x), np.arange(M)+1));
+print(np.shape(phi))
 
 for i in range(training_steps):
     imod = np.mod(i,N);
-    wml = wml + 1e-11*np.sum(np.mat(np.array(y - np.transpose(wml)*phi)*np.array(phi)),1);
+    coef = np.array(y[:, imod] - np.transpose(wml)*phi[:, imod]);
+    wml = (wml + (1e-9)*coef[0,0]*phi[:, imod]); 
+    print(wml)
+    #print(np.shape(np.mat(np.array(y[imod,:] - np.transpose(wml)*phi[imod,:])*np.array(phi))))
+    #wml = wml + (1e-11)*np.mat(np.array(y - np.transpose(wml)*phi[imod,:])*np.array(phi));
     #wml = wml + 0.01*(y[imod] - np.transpose(wml)*phi)*phi;
     #wml = wml + 0.0000001*twphi[0,0]*phi;
     #print(wml)
