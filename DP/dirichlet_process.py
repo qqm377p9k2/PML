3c0b55f55854588f4f7d1c413e79a15dadc9f57c
Taichi <taichi@dhcp-169-245.iis.u-tokyo.ac.jp>
2013-08-28 06:24:01 +0900

    simple CRP has been implemented

--- DP/dirichlet_process.py
+++ DP/dirichlet_process.py
@@ -9,14 +9,49 @@ class draw:
     cnts = []
     theta = []
     alpha = 0.01
-    def train(self, data):
+
+class draw10w:
+    """A draw from a diriclet process only with 10 words"""
+    cnts = []
+    theta = []
+    alpha = 0.01
+    noWords = 10
+    def CRP(self, data):
         assert(self.alpha>0)
-    def posterior(self):
-        self.theta
+        data = self.processData(data)
+        for i in range(len(data)):
+            while True:
+                table = np.random.multinominal(self.posterior(data[i]))
+                if table[-1] == 1:       #a new table is organized
+                    self.cnts.append(0)
+                    self.theta.append(diri.npSampleDirichlet(1,self.noWords))
+                else:                    #guide the customer to the prefered table and exit while
+                    self.cnts += table[:-1]
+                    break
+
+    def processData(self, data):
+        assert(len(data.shape) == 1)
+        assert(max(data)<self.noWords)
+        assert(min(data)>=0)
+        matrix = np.arange(self.noWords)
+        return [element == matrix for element in data] #convert data to 1-of-10 expression
+
+    def posterior(self, samples):
+        """samples are in 1-of-10 expression"""
+        samples = np.array(samples)
+        assert(len(samples.shape) == 1)
+        assert(np.max(samples)==1)
+        assert(np.min(samples)==0)
+        assert(np.sum(samples)==1)
+        posterior = [cnt*l for l in [diri.npPDFDirichlet(t, samples+1) for t in self.theta]]
+        posterior = [pos/(sum(posterior)+self.alpha) for pos in posterior]
+        posterior.append(self.alpha/(sum(posterior)+self.alpha))
+        assert(sum(posterior)==1)
+        return np.array(posterior)
 
 
 def main():
-    diri.npSampleDirichlet(1,(3,10))
+    diri.npSampleDirichlet(1,(10,20))
 
 if __name__=="__main__":
     main()
