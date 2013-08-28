9a1048c35458991a6eee668a06fac6d2025b7342
Taichi <taichi@dhcp-169-245.iis.u-tokyo.ac.jp>
2013-08-28 09:14:05 +0900

    added a routine for extracting words from Brown corpus

--- DP/dirichlet_process.py
+++ DP/dirichlet_process.py
@@ -1,21 +1,22 @@
 import numpy as np
 import nltk
+from nltk.corpus import brown
 import dirichlet as diri
 import matplotlib.pyplot as plt
-from nltk.corpus import brown
+
 
 class draw:
     """A draw from a diriclet process """
     cnts = []
     theta = []
-    alpha = 0.01
+    alpha = 0.1
 
-class draw10w:
-    """A draw from a diriclet process only with 10 words"""
+class drawSmall:
+    """A draw from a diriclet process only with relative small number of words"""
     cnts = []
     theta = []
-    alpha = 0.01
-    noWords = 10
+    alpha = 0.1
+    noWords = 150
     def CRP(self, data):
         assert(self.alpha>0)
         data = self.processData(data)
@@ -28,6 +29,9 @@ class draw10w:
                 else:                    #guide the customer to the prefered table and exit while
                     self.cnts += table[:-1]
                     break
+
+    def __init__(self, alpha=0.1):
+        self.alpha = alpha
 
     def processData(self, data):
         assert(len(data.shape) == 1)
@@ -43,15 +47,30 @@ class draw10w:
         assert(np.max(samples)==1)
         assert(np.min(samples)==0)
         assert(np.sum(samples)==1)
-        posterior = [cnt*l for l in [diri.npPDFDirichlet(t, samples+1) for t in self.theta]]
+        likelihood = [diri.npPDFDirichlet(t, samples+1) for t in self.theta]
+        posterior = [cnt*l for l in likelihood]
         posterior = [pos/(sum(posterior)+self.alpha) for pos in posterior]
         posterior.append(self.alpha/(sum(posterior)+self.alpha))
         assert(sum(posterior)==1)
         return np.array(posterior)
 
+def extractData(self):
+    """
+    extract words from brown corpus to compose a vector expression like
+    [0,1,3,1,2,0]
+    """
+    news_text = brown.words(categories="news")
+    fdist = nltk.FreqDist([w.lower() for w in news_text])
+    words = fdist.samples()[10:6000:40]
+    data = [w for w in news_text if w in words]
+    return  [words.index(d) for d in data]
+
 
 def main():
-    diri.npSampleDirichlet(1,(10,20))
+    #diri.npSampleDirichlet(1,(10,20))
+    draw = draw10w()
+    data = draw.processData(draw.extractData())
+    draw.CRP(data)
 
 if __name__=="__main__":
     main()
