7715a02b21adeb2c2a4d74ba152b4d0bd11df0c6
Taichi Kiwaki <taichi@roganjosh.local>
2013-08-30 14:02:26 +0900

    made the class abstract

--- DP/baseDist.py
+++ DP/baseDist.py
@@ -1,5 +1,8 @@
 class baseDist:
     """An abstract class for base distribution"""
+    def __init__(self):
+        assert(False)   #instance of baseDist can not be made
+
     def sample(self):
         pass
 
