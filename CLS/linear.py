69c5cced03e822a61661237582a77bf58c1d3721
Taichi Kiwaki <taichi@dhcp-169-196.iis.u-tokyo.ac.jp>
2013-09-09 23:32:15 +0900

    correction on import statements

--- CLS/linear.py
+++ CLS/linear.py
@@ -1,5 +1,11 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import __builtin__ as base
+import GMM
 from numpy import *
 from numpy.random import *
 
+def main():
+    pass
 
+if __name__=="__main__":
+    main()
