891ba1d60d19c61f1195023984daf77ef2bf2352
Taichi Kiwaki <qqm377p9k@gmail.com>
2013-09-13 15:38:23 +0900

    display with decision planes

--- CLS/mLinear.py
+++ CLS/mLinear.py
@@ -29,14 +29,31 @@ class mLinear(object):
         print(w)
         print(tmp)
         print(b)
-        p = dot(b,LA.inv(tmp))
-        print(p)
-        return (p[:-1], p[-1])
+        self.p = dot(b,LA.inv(tmp))
+        self.val = -self.p[-1]
+        self.p = tuple(self.p[:-1])
+        return (self.p[:], self.val)
+
+    def border(self, x, target=(0,1)):
+        wml = self.wml()
+        w = wml[:,target[0]]-wml[:,target[1]]
+        y = -(w[0]*x+w[2])/w[1]
+        lim = dot(c_[x, y, ones(len(x))], wml[:,target[0]])>self.val
+        return (x[lim],y[lim])
+
+    def borders(self, x, target=0):
+        wml = self.wml()
+        wset = [wml[:,0]-wml[:,1], wml[:,0]-wml[:,2]]
+        ys = [-(w[0]*x+w[2])/w[1] for w in wset]
+
+        lim = dot(c_[x, ys[0], ones(len(x))], wml[:,0])>self.val
+        return (x[lim],ys[0][lim])
+    
 
         
 def main():
     gmm = GMM(N=1000)
-    gmm.append(natDist(array([0.,10.]), 
+    gmm.append(natDist(array([-10.,5.]), 
                        array([[5.,0.],
                               [0.,3.]])),
                0.4)
@@ -60,13 +77,18 @@ def main():
     plt.xlim(xlim[0]-3,xlim[1]+3)
     plt.ylim(ylim[0]-3,ylim[1]+3)
     plt.plot(tics, cls.classifiers[0].cPlane(tics),
-             color='black')
+             color='gray')
     plt.plot(tics, cls.classifiers[1].cPlane(tics),
-             color='black')
+             color='gray')
     plt.plot(tics, cls.classifiers[2].cPlane(tics),
-             color='black')
+             color='gray')
     p, val = cls.crossPoint()
-    plt.scatter(p[0], p[1], color='orange')
+    plt.scatter(*p, color='black')
+    plt.plot(*cls.border(tics, target=(0,1)), color='black')
+    plt.plot(*cls.border(tics, target=(1,2)), color='black')
+    plt.plot(*cls.border(tics, target=(2,0)), color='black')
+    
+    #plt.plot()
     plt.show()
 
 
